8c39b4b2d870357f989d49436d6b7805760f0a1c
Pau-za <molo_pau@hotmail.com>
2020-03-07 15:31:06 -0600

    ending first question

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.sidenav').sidenav();
 });
+
+// add event checked try with change event
+
+const question1 = document.getElementsByClassName('q1');
+console.log(question1);
+
+for (let i = 0; i < question1.length; i ++) {
+    question1[i].addEventListener('change', () => {
+        console.log(question1[i].value);
+    })
+}
