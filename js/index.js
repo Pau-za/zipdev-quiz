aa630d05006e51102e97663f9da2798ceb966ef7
Pau-za <molo_pau@hotmail.com>
2020-03-07 16:57:56 -0600

    creates object with users answers and adds first one to it

--- js/index.js
+++ js/index.js
@@ -6,13 +6,56 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
 });
 
-// add event checked try with change event
+//hacer un objeto que guarde toda la informaci{on del usuario para pintarla al final del quiz}
 
-const question1 = document.getElementsByClassName('q1');
-console.log(question1);
+const userAnswers = {
+    'q1': '',
+    'q2': '',
+    'q3': '',
+    'q4': ''
+};
 
-for (let i = 0; i < question1.length; i ++) {
-    question1[i].addEventListener('change', () => {
-        console.log(question1[i].value);
+const get1stQ = () => {
+    const question1 = document.getElementsByClassName('q1');
+    for (let i = 0; i < question1.length; i++) {
+        if (question1[i].checked == true) {
+            // add info to the object;
+            userAnswers.q1 = question1[i].value;
+            console.log(userAnswers);
+        }
+    };
+};
+
+const btns = document.getElementsByTagName('button');
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        const specificBtn = btns[i].name;
+        if (specificBtn == 2) {
+            //cambiar a pregunta 2
+            //obtener el radiobutton seleccionado
+            get1stQ();
+        }
     })
-}
+};
+
+// --------------------- funci{on para barra de progreso}
+// let i = 0;
+// let width = 25;
+// const move = () => {
+//   if (i == 0) {
+//     i = 1;
+//     const elem = document.getElementById("myBar");
+//     const frame = () => {
+//       if (width >= 50) {
+//         clearInterval(id);
+//         i = 0;
+//       } else {
+//         width++;
+//         elem.style.width = width + "%";
+//       }
+//     };
+//     const id = setInterval(frame, 50);
+//   }
+// }
